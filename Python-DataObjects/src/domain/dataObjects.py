b2f46180e76f1ef8128679c63a2ba29a1970f2da
paulocheque <paulocheque@cc04c64a-0f25-11de-8837-45bce846067a>
2009-03-12 17:35:00 +0000

    Initial import.

---
+++ Python-DataObjects/src/domain/dataObjects.py
@@ -0,0 +1,132 @@
+'''
+Created on Mar 8, 2009
+
+@author: Paulo Cheque (paulocheque@agilbits.com.br)
+'''
+
+from domain import validator
+
+class A(object): pass
+class B(A): pass
+
+class DataObject(object):
+  '''
+  An data object with useful methods for validation
+  
+  Example of usage:
+  
+  import dataObject
+  
+  class MyEntity(dataObject.Entity):
+    
+    def __init__(self):
+      self.someint = 1
+      self.somefloat = 1.23
+      self.somestring = 'abc'
+      self.somelist = [1, 2]
+      self.somedict = {1:1, 2:2}
+      self.sometuple = (1, 2)
+      
+  MyEntity.addConstraints('someint', Nullable = False, Min = 3, Max = 4, InList = [1, 2], Custom = lambda x: x == 1)
+  MyEntity.addConstraints('somefloat', Nullable = False, Min = 3, Max = 4, Scale = 4, InList = [1.0, 2.0], Custom = lambda x: x == 1.0)
+  MyEntity.addConstraints('somestring', Nullable = False, Min = 3, Max = 4, InList = ['a', 'b'], Matches = '[0-9]', Email = True, IP = True, Site = True, Custom = lambda x: x == 'a')
+  MyEntity.addConstraints('somelist', Nullable = False, Min = 3, Max = 4, InList = [[1, 2]], Custom = lambda x: x == [1, 2])
+  MyEntity.addConstraints('somedict', Nullable = False, Min = 3, Max = 4, InList = [{1:1, 2:2], Custom = lambda x: x == {1:1, 2:2})
+  MyEntity.addConstraints('sometuple', Nullable = False, Min = 3, Max = 4, InList = [(1, 2)], Custom = lambda x: x == (1, 2))
+  
+  class MyValueObject(dataObject.ValueObject):
+
+    def __init__(self, somevariable):
+      self.somevariable = somevariable
+  
+  MyValueObject.addConstraints('somevariable', Nullable = False, Min = 3)    
+  
+  # Now, run:
+  
+  example = MyEntity()
+  print(example.valid())
+  print(example.hasErrors())
+  print(example.errors))
+  
+  print(MyValueObject(3) == MyValueObject(3))
+  print(MyValueObject(3) == MyValueObject(4))
+  print(MyValueObject(2).valid())
+  print(MyValueObject(3).valid())
+  '''
+  
+  constraints = {}
+
+  @classmethod
+  def addConstraints(clazz, attributeName, **attrConstraints):
+    parentClass = clazz.__mro__[1]
+    if id(parentClass.constraints) == id(clazz.constraints):
+      clazz.constraints = {}
+      clazz.constraints.update(parentClass.constraints)
+    clazz.constraints[attributeName] = attrConstraints
+
+  def __getValue(self, attributeName):
+    try:
+      return eval('self.' + attributeName)
+    except AttributeError:
+      raise validator.ConstraintException('Constraint error: Invalid attribute')
+
+  def __validateInnerDataObject(self, var):
+    eval('self.' + var + '.validate()')
+    innerErrors = eval('self.' + var + '.errors()')
+    self.__currentErrors.extend(innerErrors)
+
+  def validate(self):
+    self.__currentErrors = []
+    for attributeName in self.constraints:
+      value = self.__getValue(attributeName)
+      if issubclass(value.__class__, DataObject):
+        self.__validateInnerDataObject(attributeName)
+      for constraintName in self.constraints[attributeName]:
+        requiredValue = self.constraints[attributeName][constraintName]
+        constraint = validator.ConstraintFactory.getConstraint(constraintName, attributeName, requiredValue, value)
+        if not constraint.valid():
+          self.__currentErrors.append(constraint.message())
+
+  def errors(self):
+    self.validate()
+    return self.__currentErrors
+  
+  def xxx(self):
+    print('self = ' + str(self))
+    print('self class = ' + str(self.__class__))
+
+  def valid(self):
+    return len(self.errors()) == 0
+
+  def hasErrors(self):
+    return not self.valid()
+  
+  def __str__(self):
+    string = self.__class__.__name__
+    variables = sorted(vars(self))
+    if len(variables) == 0: return string
+    string += ': '
+    for var in variables:
+      string += (var + '=(' + str(eval('self.' + var)) + '), ')
+    return string[0:len(string)-2]
+
+class ValueObject(DataObject): 
+  '''
+  Just use to document the data object
+  '''
+  def __eq__(self, that):
+    if isinstance(that, self.__class__):
+      return vars(self) == vars(that)
+    return False
+  
+  def __ne__(self, that):
+    return not self.__eq__(that)
+
+class Entity(DataObject): 
+  '''
+  Just use to document the data object
+  Entity has an identifier
+  '''
+#  def __init__(self, id=None):
+#    self.id = id
+  pass
---
+++ Python-DataObjects/src/domain/validator.py
@@ -0,0 +1,226 @@
+'''
+Created on Mar 8, 2009
+
+@author: Paulo Cheque (paulocheque@agilbits.com.br)
+
+To register a new constraint, follow:
+
+from domain import validator
+
+class MyConstraint(validator.Constraint):
+  def valid(self):
+    return True # bool value
+
+  def message(self): return 'some text (self.attributeName, self.value, self.requiredValue)'
+  
+MyConstraint.load()
+
+Now, it is possible to do this:
+
+MyEntity.addConstraints('somevariable', My = True)
+'''
+
+import re
+from string import Template
+
+class ConstraintException(Exception):
+  '''
+  classdocs
+  '''
+  
+  def __init__(self, value):
+    '''
+    constructor
+    '''
+    self.value = value
+    
+  def __str__(self):
+    return repr(self.value)
+
+
+class Constraint(object):
+  
+  def __init__(self, attributeName, requiredValue, value):
+    self.attributeName = attributeName
+    self.requiredValue = requiredValue
+    self.value = value
+  
+  def load(clazz):
+    '''
+    Just a method to facilitate the developers work. 
+    Cycle dependency but it is ok for a good reason.
+    '''
+    ConstraintFactory.addConstraint(clazz)
+  load = classmethod(load)
+  
+  def getName(clazz):
+    return clazz.__name__.replace('Constraint', '')
+  getName = classmethod(getName)
+  
+  def valid(self): pass
+  
+  def message(self): pass
+  
+class ConstraintFactory(object):
+  
+  constraintsRules = []
+  
+  def addConstraint(constraintClass):
+    if not issubclass(constraintClass, Constraint):
+      raise ConstraintException('Invalid Constraint, please, extend Constraint class') 
+    ConstraintFactory.constraintsRules.append(constraintClass.getName())
+  addConstraint = staticmethod(addConstraint)
+  
+  def getConstraint(name, attributeName, requiredValue, value):
+    if name in ConstraintFactory.constraintsRules:
+#      return eval('from domain import validatorTest')
+      return eval(name + 'Constraint(attributeName, requiredValue, value)')
+    else:
+      raise ConstraintException('Constraint ' + name + 'Constraint not registered')
+  getConstraint = staticmethod(getConstraint)
+  
+class MinConstraint(Constraint):
+  
+  def valid(self):
+    if self.value is None: return False
+    if isinstance(self.value, (str, list, dict, tuple)):
+      return len(self.value) >= self.requiredValue
+    return self.value >= self.requiredValue
+  
+  def message(self):
+    if isinstance(self.value, (str, list, dict, tuple)):
+      t = Template('$attr (= $value) must have length greater or equal than $required')
+    else:
+      t = Template('$attr (= $value) must be greater or equal than $required')
+    return t.substitute(attr=self.attributeName, value=self.value, required=self.requiredValue)
+  
+MinConstraint.load()
+  
+class MaxConstraint(Constraint):
+  
+  def valid(self):
+    if self.value is None: return False
+    if isinstance(self.value, (str, list, dict, tuple)):
+      return len(self.value) <= self.requiredValue
+    return self.value <= self.requiredValue
+  
+  def message(self):
+    if isinstance(self.value, (str, list, dict, tuple)):
+      t = Template('$attr (= $value) must have length lower or equal than $required')
+    else:
+      t = Template('$attr (= $value) must be lower or equal than $required')
+    return t.substitute(attr=self.attributeName, value=self.value, required=self.requiredValue)
+  
+MaxConstraint.load()
+  
+class NullableConstraint(Constraint):
+  
+  def valid(self):
+    if self.requiredValue: return True
+    else: return self.value is not None
+    
+  def message(self):
+    t = Template('$attr (= $value) must be different of None')
+    return t.substitute(attr=self.attributeName, value=self.value, required=self.requiredValue)
+  
+NullableConstraint.load()
+    
+class MatchesConstraint(Constraint):
+
+  def valid(self):
+    if not isinstance(self.value, str): return False
+    return re.match(self.requiredValue, self.value) is not None
+  
+  def message(self):
+    t = Template('$attr (= $value) must matches $required')
+    return t.substitute(attr=self.attributeName, value=self.value, required=self.requiredValue)
+
+MatchesConstraint.load()
+
+class InListConstraint(Constraint):
+
+  def valid(self):
+    return self.value in self.requiredValue
+  
+  def message(self):
+    t = Template('$attr (= $value) must be in list $required')
+    return t.substitute(attr=self.attributeName, value=self.value, required=self.requiredValue)
+  
+InListConstraint.load()
+
+class ScaleConstraint(Constraint):
+
+  def valid(self):
+    if not isinstance(self.value, float): return False
+    return len(re.sub('[0-9][.]', '', str(self.value))) <= self.requiredValue
+  
+  def message(self):
+    t = Template('$attr (= $value) must have $required decimals or less')
+    return t.substitute(attr=self.attributeName, value=self.value, required=self.requiredValue)
+
+ScaleConstraint.load()
+
+class EmailConstraint(MatchesConstraint):
+
+  def valid(self):
+    '''
+    TODO need better regular expression
+    '''
+    if not self.requiredValue: return True
+    if not isinstance(self.value, str): return False
+    matches = MatchesConstraint(self.attributeName, '^.+[@].+[.].{1,4}$', self.value)
+    return matches.valid()
+
+  def message(self):
+    t = Template('$attr (= $value) must be a valid e-mail address')
+    return t.substitute(attr=self.attributeName, value=self.value, required=self.requiredValue)
+
+EmailConstraint.load()
+
+class IPConstraint(MatchesConstraint):
+
+  def valid(self):
+    if not self.requiredValue: return True
+    if not isinstance(self.value, str): return False
+    matches = MatchesConstraint(
+      self.attributeName, 
+      '^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])[.]){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]){1}$', 
+      self.value)
+    return matches.valid()
+    
+  def message(self):
+    t = Template('$attr (= $value) must be a valid ip address')
+    return t.substitute(attr=self.attributeName, value=self.value, required=self.requiredValue)
+
+IPConstraint.load()
+
+class SiteConstraint(MatchesConstraint):
+
+  def valid(self):
+    '''
+    TODO need better regular expression
+    '''
+    if not self.requiredValue: return True
+    if not isinstance(self.value, str): return False
+    matches = MatchesConstraint(self.attributeName, '^(http|https)[:][/][/].+$', self.value)
+    return matches.valid()
+    
+  def message(self):
+    t = Template('$attr (= $value) must be a valid site address')
+    return t.substitute(attr=self.attributeName, value=self.value, required=self.requiredValue)
+
+SiteConstraint.load()
+  
+class CustomConstraint(MatchesConstraint):
+
+  def valid(self):
+    return self.requiredValue(self.value)
+  
+  def message(self):
+    '''
+    TODO is it possible to print the lambda function?
+    '''
+    t = Template('$attr (= $value) must be satisfied by specific function')
+    return t.substitute(attr=self.attributeName, value=self.value, required=self.requiredValue)
+
+CustomConstraint.load()
